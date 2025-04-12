9bfb3223ffe56a4ff3d377568d41352bc6995a5f
kathiroli21 <kathiroli216@gmail.com>
2025-04-12 13:56:36 +0530

    updated package and middleware

---
+++ config/mongoDbConfig.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log("MongoDB URI:", process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL);
+    const connection = mongoose.connection;
+    console.log("connected");
+
+    connection.on("error", (error) => {
+      console.log("Something is wrong in MongoDB", error);
+    });
+  } catch (error) {
+    console.log("Something is wrong", error);
+  }
+};
+
+export default connectDB;
